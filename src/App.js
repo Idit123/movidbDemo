db3fe723aae60342de890cfa48c959b5853b04be
Idit Talaviya <iditlogisticinfotech@gmail.com>
2021-12-29 19:27:59 +0530

    29/12/2021

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import Router from "./Routes"
 import "./App.css"
 import React, { useEffect } from "react"
-import store from "./redux/store"
+import AllStore from "./redux/store"
+// import { saveState } from "./redux/localStorage"
 
-const App = (props) => {
+const App = () => {
   // Life cycle Hooks
   useEffect(() => {
-    console.log("App is mounted with store data", store.getState())
+    console.log("App is mounted with store data", AllStore.store.getState())
   }, [])
+
+  // store.subscribe(() => {
+  //   saveState({
+  //     /* example state */
+  //     scrollIndex: store.getState().scrollIndex,
+  //     movie: store.getState().movie,
+  //     tvShow: store.getState().tvShow,
+  //   })
+  // })
 
   return (
     <>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,36 @@
 import { createStore, applyMiddleware } from "redux"
 import { composeWithDevTools } from "redux-devtools-extension"
 import createSagaMiddleware from "redux-saga"
-
 import rootReducer from "./reducer"
 import rootSaga from "./saga"
+// import { loadState } from "./localStorage"
+import { persistStore, persistReducer } from "redux-persist"
+import storage from "redux-persist/lib/storage"
 
+// const persistedState = loadState()
+
+//config persistStore
+const persistConfig = {
+  key: "root",
+  storage,
+}
+
+//persistReducer
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+//create sagaMiddleware
 const sagaMiddleware = createSagaMiddleware()
 
+//create store
 const store = createStore(
-  rootReducer,
+  persistedReducer,
+  // persistedState,
   composeWithDevTools(applyMiddleware(sagaMiddleware))
 )
+//create persistStore
+const persistor = persistStore(store)
 
+//run sagaMiddleware
 sagaMiddleware.run(rootSaga)
 
-export default store
+export default { store, persistor }
